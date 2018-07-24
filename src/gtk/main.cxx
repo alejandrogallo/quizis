e78db09120fc6e3c95162d02883e6896d43f3904
Alejandro Gallo <aamsgallo@gmail.com>
2018-07-24 02:00:26 +0200

    Update gtk

--- src/gtk/main.cxx
+++ src/gtk/main.cxx
@@ -1,7 +1,30 @@
+#include <iostream>
 #include <gtkmm.h>
+#include <gtkmm/button.h>
 
 
+class QuizisWindow : public Gtk::Window
+{
+public:
+  QuizisWindow(){
+    set_border_width(10);
+    set_default_size(200, 200);
+    add(hbox);
 
+    Gtk::Button *button = new Gtk::Button("hello");
+    Gtk::Button *button2 = new Gtk::Button("world");
+    hbox.add(*button);
+    hbox.add(*button2);
+
+    hbox.show_all();
+
+  };
+  ~QuizisWindow(){};
+
+protected:
+  Gtk::VBox hbox;
+
+};
 
 
 
@@ -10,8 +33,9 @@ int main(int argc, char *argv[])
 {
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
-  Gtk::Window window;
-  window.set_default_size(200, 200);
+  QuizisWindow window;
+
 
   return app->run(window);
+
 }
